cfd022d7881761b2c7346cc04e4c87e3e0fbb486
Hans Wannop <hanswannop@me.com>
2019-07-10 19:52:38 +0200

    Start on AI guard, spawn black hole post mission.

--- Source/FPSGame/Private/FPSBlackHole.cpp
+++ Source/FPSGame/Private/FPSBlackHole.cpp
@@ -37,7 +37,7 @@ void AFPSBlackHole::BeginPlay()
 
 void AFPSBlackHole::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor)
+	if (OtherActor && OtherActor != this)
 	{
 		PlayEffects();
 		OtherActor->Destroy();
